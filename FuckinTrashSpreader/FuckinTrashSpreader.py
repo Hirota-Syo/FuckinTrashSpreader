aa1b2502109c0f72a023ec976d54b10bec08600d
Hirota-Syo <hirota.syo.0327@gmail.com>
2023-06-05 11:44:47 +0900

    first commit

---
+++ FuckinTrashSpreader/FuckinTrashSpreader.py
@@ -0,0 +1,20 @@
+from libs import module_sample
+# --- write import here ↓ --- 
+
+###########################################
+# func  : **
+# arg   : arg1       :**
+# arg   : arg2       :**
+# ret   : **
+###########################################
+def main():
+    # --- write instance here ↓ --- 
+    obj_sample = module_sample.csample()
+    
+    # --- write code here ↓ --- 
+    obj_sample.func()
+    print(obj_sample.isample)
+    return
+
+if __name__ == "__main__":
+    main()
